2695731849bfba228b8703673c1c36feef255414
Carlo <romerocarlodaniel@gmail.com>
2014-09-17 19:27:40 -0700

    Tercer commit del proyecto blog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,19 @@
 class BlogController < ApplicationController
 	def index
-		#inicio listos para codear
-		@mensaje = 'Hola desde el controlador'
+		#crear un registro
+		registro = Mensaje.new
+		registro.titulo="Primer titulo"
+		registro.descripcion="Mi primera descripcion"
+		registro.save
+
+		#manda el primer registro de la bd
+		@mensaje = Mensaje.first
+
+		#manda la cantidad de registros en la tabla mensajes
+		@cantidad = Mensaje.count
+
+		#Hora y fecha
+		@currentTime = Time.now
 	end
 
 end
